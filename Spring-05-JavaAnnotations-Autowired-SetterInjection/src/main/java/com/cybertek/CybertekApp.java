bcbb4ba9223fc4ecebc565633fc99dbea722fbad
ozzy <ozzy@cybertekschool.com>
2020-10-02 20:37:40 -0400

    JavaAnnotations-Autowired-SetterInjection

---
+++ Spring-05-JavaAnnotations-Autowired-SetterInjection/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,27 @@
+package com.cybertek;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
+
+public class CybertekApp {
+
+	public static void main(String[] args) {
+			
+
+		ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+		Course course = (Course) container.getBean("java");
+		
+		course.getTeachingHours();
+		
+		
+		
+		
+	}
+
+}
---
+++ Spring-05-JavaAnnotations-Autowired-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,30 @@
+package com.cybertek.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+@Component
+public class Java implements Course {
+	
+	private ExtraSessions extraSessions;
+	
+	
+	@Autowired
+	public void setExtraSessions(ExtraSessions extraSessions) {
+		this.extraSessions = extraSessions;
+	}
+
+
+	@Override
+	public void getTeachingHours() {
+		System.out.println("Weekly Teaching Hours: " + (20 + extraSessions.getHours()));
+	}
+
+}
+
+
+
+

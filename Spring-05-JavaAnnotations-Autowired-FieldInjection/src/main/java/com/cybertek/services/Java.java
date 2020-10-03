f27d141075ae0fc83f346da9666ed3137f5d5170
ozzy <ozzy@cybertekschool.com>
2020-10-02 20:36:29 -0400

    JavaAnnotations-Autowired-FieldInjection

---
+++ Spring-05-JavaAnnotations-Autowired-FieldInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,25 @@
+package com.cybertek.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+@Component
+public class Java implements Course {
+	
+	@Autowired
+	private ExtraSessions extraSessions;
+	
+
+	@Override
+	public void getTeachingHours() {
+		System.out.println("Weekly Teaching Hours: " + (20 + extraSessions.getHours()));
+	}
+
+}
+
+
+
+
